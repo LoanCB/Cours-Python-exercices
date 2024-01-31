72502d70785e0a459792fd490e9966817442b536
Loan <loanbillonnet@gmail.com>
2024-01-31 19:31:36 +0100

    feat: :sparkles: exercices about operations

---
+++ 2_types/operations.py
@@ -0,0 +1,18 @@
+VALUE_1 = 5
+VALUE_2 = 3
+
+addition = VALUE_1 + VALUE_2
+soustraction = VALUE_1 - VALUE_2
+multiplication = VALUE_1 * VALUE_2
+puissance = VALUE_1 ** VALUE_2
+division = VALUE_1 / VALUE_2
+quotient = VALUE_1 // VALUE_2
+reste = VALUE_1 % VALUE_2
+
+print(addition)
+print(soustraction)
+print(multiplication)
+print(puissance)
+print(division)
+print(quotient)
+print(reste)
