3cceed934d0920d6c59484a7b284eab74dd9feee
Loan <loanbillonnet@gmail.com>
2024-02-01 19:55:28 +0100

    feat: :sparkles: exercices about list manipulations

---
+++ 3_complex_types/list.py
@@ -0,0 +1,14 @@
+european_countries = ['France', 'United-kingdom', 'Italy']
+first_numbers = (2, 3, 5, 7, 11, 13)
+choices = set([42, 7, 18, 4])
+
+print(european_countries)
+print(type(european_countries))
+print('\n')
+
+print(first_numbers)
+print(type(first_numbers))
+print('\n')
+
+print(choices)
+print(type(choices))
---
+++ 3_complex_types/manipulating_list.py
@@ -0,0 +1,10 @@
+fruits = ['banane', 'abricot', 'kiwi', 'orange', 'clémentine', 'poire', 'pomme', 'litchi', 'fraise', 'fruit de la passion']
+
+print(fruits[0])
+print(fruits[-1])
+print(fruits[0:5])
+print(fruits.index('orange'))
+print('framboise' in fruits)
+print(len(fruits))
+print(min(fruits))
+print(fruits[1::2])
