4b81f82771dd9f301ddf1071bf3e32532b4b2092
Loan Courchinoux-Billonnet <74971600+Patais@users.noreply.github.com>
2024-02-21 14:50:52 +0100

    docs: :bulb: show another way to calc power on hypotenuse exercice

--- 2_types/hypotenuse.py
+++ 2_types/hypotenuse.py
@@ -3,6 +3,6 @@ import math
 ab = float(input("Valeur de AB : "))
 ac = float(input("Valeur de AC : "))
 
-bc_carre = ab ** 2 + ac ** 2 # OU bc_carre = pow(ab, 2) + pow(ac, 2)
+bc_carre = ab ** 2 + ac ** 2  # OU bc_carre = pow(ab, 2) + pow(ac, 2)
 bc = math.sqrt(bc_carre)
 print(bc)
