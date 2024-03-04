fdbcef6b94c74437ebb336c398ae10cd23034084
Loan Courchinoux-Billonnet <74971600+Patais@users.noreply.github.com>
2024-03-04 22:45:59 +0100

    feat: :sparkles: first scripts on loops chapter

---
+++ 5_loops/for_range.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print(i)
+
+for i in range(2, 6):
+    print(i)
+
+for i in range(2, 10, 2):
+    print(i)
+
+for i in range(5, 0, -1):
+    print(i)
---
+++ 5_loops/list_loop.py
@@ -0,0 +1,4 @@
+fruits = ['banane', 'abricot', 'kiwi', 'orange', 'clémentine', 'poire', 'pomme', 'litchi', 'fraise', 'fruit de la passion']
+
+for fruit in fruits:
+    print(fruit)
